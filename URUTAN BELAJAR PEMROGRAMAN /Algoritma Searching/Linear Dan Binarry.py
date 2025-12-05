9efb3abbd94e099f558e426b02bfb1ff3d6f2ce0
ibrahimsisfo <ibrahimsisfo@gmail.com>
2025-12-05 15:53:45 +0800

    Add files via upload

---
+++ URUTAN BELAJAR PEMROGRAMAN /Algoritma Searching/Linear Dan Binarry.py
@@ -0,0 +1,24 @@
+nama_program="Linear search"
+print(nama_program)
+data = [11, 6, 2, 7, 8, 5]
+
+cari = 6
+for i in range(len(data)):
+    if data[i] == cari:
+        print("data ditemukan di indeks", i)
+
+nama_program="Binarry search"
+print(nama_program)
+data = [4, 6, 5, 3, 1]
+cari = 5
+
+low, high = 0, len(data)-1
+while low <= high:
+    mid = (low + high)//2
+    if data[mid] == cari:
+        print("data ditemukan di indeks", mid)
+        break
+    elif data[mid] < cari:
+        low = mid + 1
+    else:
+        high = mid - 1
