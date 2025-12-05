7ec55d9f5bba2ac4e8762b751ebd68be7611cd2e
ibrahimsisfo <ibrahimsisfo@gmail.com>
2025-12-05 10:22:42 +0800

    Add files via upload

---
+++ URUTAN BELAJAR PEMROGRAMAN /Tipe Data, Array, Struktur, Unions, Variabel Statis & Variabel Dinamis/variabelstatis&dinamis.py
@@ -0,0 +1,13 @@
+#Dinamis
+x = 10
+x = "Hallo"  # berubah tipe
+print(x)
+
+#Statis
+class Contoh:
+    counter = 0  # variabel statis
+
+c1 = Contoh()
+c2 = Contoh()
+Contoh.counter += 2006
+print(Contoh.counter)
