86580724c962b7e2facfb739adde38a42203514c
ibrahimsisfo <ibrahimsisfo@gmail.com>
2025-12-11 13:10:35 +0800

    Add files via upload

---
+++ FINAL TEST/6. Type data utama atau umumnya sering dipakai/Type data atau umumnya sering dipakai.py
@@ -0,0 +1,42 @@
+#IBRAHIM
+#D0425013
+#UAS PEMROGRAMAN
+
+nama_program="String (teks)"
+print(nama_program)
+nama="ibrahim"
+print(nama)
+print("-------------------------")
+nama_program="Integer (bilangan bulat)"
+print(nama_program)
+umur = 19
+print(umur)
+print("-------------------------")
+nama_program="Float (bilangan pecahan)"
+print(nama_program)
+tinggi = 165.5
+print(tinggi)
+print("-------------------------")
+nama_program="List"
+print(nama_program)
+hobi="edit/cinematic", "main motor", "ibadah"
+print(hobi)
+print("-------------------------")
+nama_program="Boolean (benar atau salah)"
+print(nama_program)
+mahasiswa= True
+print("(mahasiswa):", mahasiswa)
+print("-------------------------")
+nama_program="TUPLE (Data tetap/Tidak dapat diubah)"
+print(nama_program)
+koordinat = (11, 33, 66, 99)
+print("Tuple (koordinat):", koordinat)
+print("-------------------------")s
+nama_program="DICTIONARRY"
+print(nama_program)
+mahasiswa = {
+    "nama": "Ibrahim",
+    "umur": 19,
+    "jurusan": "sistem informasi"
+}
+print("(mahasiswa):", mahasiswa)
