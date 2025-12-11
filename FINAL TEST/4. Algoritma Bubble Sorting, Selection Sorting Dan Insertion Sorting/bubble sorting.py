1e82bde0a157d8b83828ed73a28112cfeb3c1626
ibrahimsisfo <ibrahimsisfo@gmail.com>
2025-12-11 13:09:31 +0800

    Add files via upload

---
+++ FINAL TEST/4. Algoritma Bubble Sorting, Selection Sorting Dan Insertion Sorting/bubble sorting.py
@@ -0,0 +1,18 @@
+#IBRAHIM
+#D0425013
+#UAS PEMROGRAMAN
+
+nama_program="BUBBLE SORTING"
+print(nama_program)
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+data = [33, 34, 45, 12, 22, 11, 97]
+print("Sebelum sorting:", data)
+print("Sesudah sorting:", bubble_sort(data))
---
+++ FINAL TEST/4. Algoritma Bubble Sorting, Selection Sorting Dan Insertion Sorting/selection sorting.py
@@ -0,0 +1,19 @@
+#IBRAHIM
+#D0425013
+#UAS PEMROGRAMAN
+
+nama_program="SELECTION SORT"
+print(nama_program)
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+data = [74, 62, 46, 6, 93, 11]
+print("Sebelum sorting:", data)
+print("Setelah sorting:", selection_sort(data))
